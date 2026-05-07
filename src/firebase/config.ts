190dd4a99fb6f8cdae259e45a2fab59561024440
Kishor Kakde Patil <kishorkakde026@gmail.com>
2026-05-07 18:43:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5305454790-ef005",
+  "appId": "1:717964187913:web:0f05a09f86ab3ee836dbd8",
+  "apiKey": "AIzaSyCIflNdccppOsW39hrxXrItREC3Je05RCI",
+  "authDomain": "studio-5305454790-ef005.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "717964187913"
+};
