ac92c0ef19f5de938ac3cf0a1cda836ce322a551
Kishor Kakde Patil <kishorkakde026@gmail.com>
2026-05-18 05:12:30 +0000

    FirebaseError: Firebase: Error (auth/invalid-api-key).     at createErro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,6 @@
+
 export const firebaseConfig = {
+  "apiKey": "AIzaSyCIflNdccppOsW39hrxXrItREC3Je05RCI",
   "projectId": "studio-5305454790-ef005",
   "appId": "1:717964187913:web:0f05a09f86ab3ee836dbd8",
   "authDomain": "studio-5305454790-ef005.firebaseapp.com",
